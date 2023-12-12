5ee5716bebc221145c481cf239023f873e97366f
Yanis Borie <yanis.borie69@gmail.com>
2023-12-12 15:34:36 +0100

    Gros changement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,6 @@ function popup(popup) {
     document.getElementById('popupbackground').style.animation = '1.25s fondu both';
     document.getElementById('popupbackground').style.display = 'flex';
     switch (popup) {
-        case "Laya":
-            document.getElementById('popupLaya').style.display = 'block';
-            break;
         case "BUT":
             document.getElementById('popupBUT').style.display = 'block';
             break;
@@ -20,16 +17,23 @@ function popup(popup) {
         case "ShoppingList":
             document.getElementById('popupShoppingList').style.display = 'block';
             break;
+        case "Seven":
+            document.getElementById('popupSeven').style.display = 'block';
+            break;
+        case "Valoroulette":
+            document.getElementById('popupValoroulette').style.display = 'block';
+            break;
     }
 }
 
 function popdown() {
-    document.getElementById('popupLaya').style.display = 'none';
     document.getElementById('popupBUT').style.display = 'none';
     document.getElementById('popupSnake').style.display = 'none';
     document.getElementById('popupCaseus').style.display = 'none';
     document.getElementById('popupPendu').style.display = 'none';
     document.getElementById('popupShoppingList').style.display = 'none';
+    document.getElementById('popupSeven').style.display = 'none';
+    document.getElementById('popupValoroulette').style.display = 'none';
     document.getElementById('popupbackground').style.display = 'none';
 
 }
